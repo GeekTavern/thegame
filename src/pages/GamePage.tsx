d2b7827afd4d6045db8579af306bfa9433335ca8
nickWoott <nicholaswootton@hotmail.co.uk>
2023-11-22 14:23:33 +0000

    installed SASS and rendered test component and ruleset

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -2,7 +2,7 @@ import { StoryPoint } from '../api/models';
 import { ChoiceDisplay } from '../components/ChoiceDisplay';
 import { ImageDisplay } from '../components/ImageDisplay';
 import { TextDisplay } from '../components/TextDisplay';
-import './gamePage.css';
+import './gamePage.scss';
 
 interface GamePageProps {
   storyPoint: StoryPoint;
@@ -12,6 +12,7 @@ interface GamePageProps {
 export const GamePage = ({ storyPoint, setStoryPoint }: GamePageProps) => {
   return (
     <div className='game-page-container'>
+      <p className='nested-paragraph'>I am an SCCS nesting test</p>
       <ImageDisplay storyPoint={storyPoint} />
       <TextDisplay storyPoint={storyPoint} />
       <ChoiceDisplay storyPoint={storyPoint} setStoryPoint={setStoryPoint} />
