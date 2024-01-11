2361bcb405d0921350b593789e5ceb9a62250366
nickWoott <nicholaswootton@hotmail.co.uk>
2024-01-11 21:58:59 +0000

    added sandbox page for pissing about with

---
+++ src/pages/SandboxPage.tsx
@@ -0,0 +1,12 @@
+const SandboxPage = () => {
+  return (
+    <div>
+      <p>
+        Once upon a time there was a lovely man who lived in the sea and liked
+        to play with other people's waste
+      </p>
+    </div>
+  );
+};
+
+export default SandboxPage;
