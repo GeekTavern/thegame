070d8b4a24123ce9cc1fc7dbb0bcea17c6c24528
nickWoott <nicholaswootton@hotmail.co.uk>
2024-02-01 19:08:16 +0000

    integrated animation library

--- src/pages/SandboxPage/SandboxPage.tsx
+++ src/pages/SandboxPage/SandboxPage.tsx
@@ -1,9 +1,30 @@
 import './sandboxPage.scss';
+import { TypeAnimation } from 'react-type-animation';
 
 const SandboxPage = () => {
+  // below we have the example from the typing animation library
+  // i have removed the cursor
   return (
     <div className='page-container'>
       <div className='box'>
+        <TypeAnimation
+          sequence={[
+            // Same substring at the start will only be typed out once, initially
+            'We produce food for Mice',
+            1000, // wait 1s before replacing "Mice" with "Hamsters"
+            'We produce food for Hamsters',
+            1000,
+            'We produce food for Guinea Pigs',
+            1000,
+            'We produce food for Chinchillas',
+            1000,
+          ]}
+          wrapper='span'
+          speed={50}
+          style={{ fontSize: '2em', display: 'inline-block' }}
+          repeat={Infinity}
+          cursor={false}
+        />
         <p>
           Once upon a time there was a lovely man who lived in the sea and liked
           to play with other people's waste
